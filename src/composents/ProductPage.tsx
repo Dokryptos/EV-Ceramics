1a3236110784d52c6eaecde7538c6d790ec9d601
Dok <pierredok@outlook.fr>
2024-02-04 19:32:21 +0100

    Add components

---
+++ src/composents/ProductPage.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent, PropsWithChildren } from "react";
+
+type Props = PropsWithChildren<{
+    title?: string,
+    index?: number
+}>
+
+
+export const ProductPage: FunctionComponent <Props> = () => {
+
+    return(
+        <>
+        </>
+    )
+}
