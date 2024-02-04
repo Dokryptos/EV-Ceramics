09516fa9fb07f9723bf282f8e32ffaa1d9de0b01
Dok <pierredok@outlook.fr>
2024-02-04 18:53:54 +0100

    Add the structure of the website

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+import { FunctionComponent } from "react";
+
+
+export const About: FunctionComponent = () => {
+
+    return(
+        <>
+        </>
+    )
+}
---
+++ src/pages/Archives.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+
+export const Archives: FunctionComponent = () => {
+
+    return(
+        <>
+        
+        </>
+    )
+}
